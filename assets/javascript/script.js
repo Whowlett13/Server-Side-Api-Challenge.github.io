e1880d8582fd127d8c73ebb0ee20caf5fe8a7911
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-03-12 11:04:33 -0400

    commiting chnages to html

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+// api key = 7290a13eddbe27c62e515aa42c9528c6
+
+
+var latLonApi = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+var weatherURL = 'https://openweathermap.org/forecast5';
+var apiKey = '7290a13eddbe27c62e515aa42c9528c6';
+var searchBtn = 
+var cityName = 
+var forecastURL = 
+
+
